08af9ee7e2b03680aeca4ae7b4df5d503464f474
sanketvishwasrao111@gmail.com <sanketvishwasrao111@gmail.com>
2024-04-11 22:41:53 +0530

    Changes in Post and Creating WelcomeMessage component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,29 @@
+import {useContext} from 'react';
+import {MdDelete} from 'react-icons/md';
+import {PostListData} from '../store/post-list-store';
+
 const Post = ({post}) => {
+  const {deletePost} = useContext (PostListData);
+
   return (
-    <div className="card" style={{width: '18rem'}}>
+    <div className="card post-card" style={{width: '30rem'}}>
       <div className="card-body">
         <h5 className="card-title">{post.title}</h5>
         <p className="card-text">
           {post.body}
+          <span
+            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+            onClick={() => deletePost (post.id)}
+          >
+            <MdDelete />
+          </span>
         </p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        {post.tags.map (tag => (
+          <span key={tag} className="badge text-bg-primary hashtag">{tag}</span>
+        ))}
+        <div className="alert alert-success reactions" role="alert">
+          This post has been reacted by {post.reactions} people.
+        </div>
       </div>
     </div>
   );
